916eb903ea5d849ae8b78064f4f028c6df665a78
Alexandra Harrison <apharri3@live.unc.edu>
2015-11-27 23:27:21 -0500

    Updated with changes

--- src/TalkingAboutNumbers.java
+++ src/TalkingAboutNumbers.java
@@ -1,37 +1,44 @@
-
-
 import java.util.*;
+
 public class TalkingAboutNumbers {
 
-	public void printWord(int n,String ch) {
-		String  one[] = {" ", " one", " two", " three", " four", " five", " six", " seven", " eight", " nine", " ten", " eleven", " twelve", " thirteen", " fourteen", " fifteen", " sixteen", " seventeen", " eighteen", " nineteen"};
+	public String printWord(int n) {
+		String test = null;
+		String  one[] = {"", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", 
+				"eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
 
-		String ten[] = {" ", " ",  " twenty", " thirty", " forty", " fifty", " sixty", "seventy", " eighty", " ninety"};
+		String ten[] = {"", "",  "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"};
 
 		if(n > 19) { 
-			System.out.print(ten[n / 10]+ " " + one[n % 10]);
+			test = ten[n / 10] + " " + one[n % 10];
 		} else {
-			System.out.print(one[n]);
+			test = one[n];
 		}
 
-		if(n > 0) {
-			System.out.print(ch);
-		}
+		return test;
 	}
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		int n = scanner.nextInt();
-		if(n == 0) {
-			System.out.print("zero");
-		}
-		TalkingAboutNumbers a = new TalkingAboutNumbers();
-		a.printWord((n / 1000000000), " hundred million");
-		a.printWord((n / 10000000) % 100, " million");
-		a.printWord(((n / 100000) % 100), " hundred thousand");
-		a.printWord(((n / 1000) % 100), " thousand");
-		a.printWord(((n / 100) % 10), " hundred");
-		a.printWord((n % 100), "");
+		String add;
 
+		while(n != -1) {
+			TalkingAboutNumbers a = new TalkingAboutNumbers();
+			if(n > 1000) {
+				add = a + " " + "thousand";
+			} 
+
+			if(n < 100000) {
+				add = a + " " + "hundred thousand";
+			}
+
+			System.out.print(a.printWord(n / 1000000000));
+			System.out.print(a.printWord((n / 10000000) % 100));
+			System.out.print(a.printWord(((n / 100000) % 100)));
+			System.out.print(a.printWord(((n / 1000) % 100)));
+			System.out.print(a.printWord(((n / 100) % 10)));
+			System.out.print(a.printWord((n % 100)));
+		}
 	}
 }
